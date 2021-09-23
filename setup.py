bb12881c8f481a69525cded3078fcefe5f5f10cf
ideapad GAMIN <rohanbagulwarrb@gmail.com>
2021-09-23 13:33:19 +0530

    setup.py alligned 2nd time

--- setup.py
+++ setup.py
@@ -7,30 +7,30 @@ PROJECT_NAME = "MLAutoRegressor"
 USER_NAME = "Rohanbagulwar"
 
 setuptools.setup(
-name=f"{PROJECT_NAME}-{USER_NAME}",
-version="0.0.2",
-author=USER_NAME,
-author_email="rohanbagulwarrb@gmail.com",
-description="its an implimentation of automatic regression",
-long_description=long_description,
-long_description_content_type="text/markdown",
-url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
-project_urls={
-"Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
-},
-classifiers=[
-"Programming Language :: Python :: 3",
-"License :: OSI Approved :: MIT License",
-"Operating System :: OS Independent",
-],
-package_dir={"": "src"},
-packages=setuptools.find_packages(where="src"),
-python_requires=">=3.7",
-install_requires=[
-"numpy",
-"pandas",
-"joblib",
-"scikit-learn"
-    
-        ]
+    name=f"{PROJECT_NAME}-{USER_NAME}",
+    version="0.0.2",
+    author=USER_NAME,
+    author_email="rohanbagulwarrb@gmail.com",
+    description="its an implimentation of automatic regression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls={
+    "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=[
+    "numpy",
+    "pandas",
+    "joblib",
+    "scikit-learn"
+        
+            ]
 )
